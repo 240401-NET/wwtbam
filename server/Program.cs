de5490d7f70d984f5c3ba008f4947c367dd28d94
JonathanTCantu <90468370+JonathanTCantu@users.noreply.github.com>
2024-04-24 19:40:31 -0500

    added notes previously deleted

--- server/Program.cs
+++ server/Program.cs
@@ -10,8 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<KsjvContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString(File.ReadAllText("ConnString.txt"))));
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
-
-
+// builder.Services.AddScoped<IGameRepository, GameRepository>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
