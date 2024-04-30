fa2bdb31eb9c365b4bc0d8183d0f0256da14a28f
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2024-04-30 17:54:00 -0400

    fetching quiz from external api + setting quiz state in game, passing currentQuestion to QuestionContainer -> mapping questionOptions

--- client/src/pages/QuestionContainer.tsx
+++ client/src/pages/QuestionContainer.tsx
@@ -1,26 +1,35 @@
-const QuestionContainer = () => {
-  return (
-  <div className="w-full h-[30vh] bg-black bg-opacity-70">  
+// import { QuestionOptionProps } from "../types"
+
+// import { useEffect } from "react"
+import { Question } from "../types"
+
+// import { useState } from "react"
+const QuestionContainer = ({ currentQuestion }: { currentQuestion: Question | null }) => {
+  // const [currentQuestion, setCurrentQuestion] = useState<QuestionOptionProps> ({
+const questionOptions: string[] = []
+  if (currentQuestion) {
+    questionOptions.push(currentQuestion.correctAnswer)
+    
+    questionOptions.push(...(currentQuestion?.incorrectAnswers ?? []))
+  }
+  console.log("currentquestion: ", currentQuestion)
+    const optionContainer = "cursor-pointer py-4 border-2 border-sky-400 rounded-xl flex items-center gap-x-8 px-16 text-xl hover:bg-amber-500 hover:text-white text-amber-500 font-bold tracking-wider"
+console.log("questionOptions: ", questionOptions)
+  const handleAnswer = () => {
+    console.log("Answer clicked")
+  }  
+return (
+  <div className="w-full h-[35vh] bg-black bg-opacity-70 ">  
     <div className="flex justify-center items-center w-1/2 rounded-xl mx-auto py-6 border-4 border-sky-400">
-      <p className="text-white text-4xl text-center font-serif font-bold">What is the capital of France?</p>
+      <p className="text-white text-4xl text-center font-serif font-bold">{currentQuestion?.question}</p>
     </div>
     <div className="grid grid-cols-2 justify-center  px-64 gap-x-8 gap-y-4 pt-6">
-    <div className="cursor-pointernpm run dev  py-4 border-2 border-sky-400 rounded-xl flex items-center gap-x-8 px-16 text-xl hover:bg-amber-500 hover:text-white text-amber-500 font-bold tracking-wider">
-        <p className="">A:</p>
-        <div className="text-white ">Question Option 1</div>
-      </div>
-      <div className="cursor-pointer py-4 border-2 border-sky-400 rounded-xl flex items-center gap-x-8 px-16 text-xl hover:bg-amber-500 hover:text-white text-amber-500 font-bold tracking-wider">
-        <p className=" ">B:</p>
-        <div className="text-white ">Question Option 2</div>
-      </div>
-      <div className="cursor-pointer  py-4 border-2 border-sky-400 rounded-xl flex items-center gap-x-8 px-16 text-xl hover:bg-amber-500 hover:text-white text-amber-500 font-bold tracking-wider">
-        <p className="">C:</p>
-        <div className="text-white ">Question Option 3</div>
-      </div>
-      <div className="cursor-pointer py-4 border-2 border-sky-400 rounded-xl flex items-center gap-x-8 px-16 text-xl hover:bg-amber-500 hover:text-white text-amber-500 font-bold tracking-wider">
-        <p className="">D:</p>
-        <div className="text-white">Question Option 4</div>
-      </div>
+      {questionOptions.length > 0 ? (questionOptions?.map((option, index) => (
+        <button className={optionContainer} key={index} onClick={handleAnswer}>
+          <p className="">A:</p>
+          <div className="text-white ">{option}</div>
+        </button>
+      ))) : null} 
     </div>
   </div>
   )
