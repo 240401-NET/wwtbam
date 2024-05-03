f28313e0f8e0636082f4d6d4c0baca7ccbdb6c15
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2024-05-02 21:03:14 -0400

    added back changes from earlier

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import mkcert from 'vite-plugin-mkcert'
+import { defineConfig } from 'vite' 
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    server: {
-        port: 5173,
-        proxy: {
-          "^/api": "http://localhost:5211"
-        }
-    },
-  plugins: [react(), mkcert()],
-})
+import react from '@vitejs/plugin-react-swc' 
+
+import mkcert from 'vite-plugin-mkcert' 
+
+ 
+
+// https://vitejs.dev/config/ 
+
+export default defineConfig({ 
+
+    server: { 
+
+        port: 5173, 
+
+        proxy: { 
+
+          "^/api": "https://wwtbam.azurewebsites.net" 
+
+        } 
+
+    }, 
+
+  plugins: [react(), mkcert()], 
+
+}) 
--- server/Dtos/NewUserDto.cs
+++ server/Dtos/NewUserDto.cs
@@ -1,6 +1,7 @@
 namespace server.Dtos;
 public class NewUserDto
 {
+    public string UserId {get;set;}
     public string UserName { get; set; }
     public string Email { get; set; }
     public string Token { get; set; }
