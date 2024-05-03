219417dee28f0bf9f9a65218061c0d2a40feb4f7
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2024-05-02 20:58:58 -0400

    changes

--- client/src/api/gameService.tsx
+++ client/src/api/gameService.tsx
@@ -1,18 +1,89 @@
-import axios from "axios";
+import axios from "axios"; 
 
-export const getQuestions = async (difficulty: string) => {
-  const response = await axios.get(
-    "https://the-trivia-api.com/api/questions",
-    {
-      params: {
-        limit: 5,
-        difficulty: difficulty,
-    }
-  }
-  );
-  const data = response.data;
-  return data;
-}
+import { GameProps } from "../types"; 
 
-//TODO: Post game when done 
-//Make a profile page w/ user info + update contact info?
+ 
+
+const baseUrl = "/api/Game"; 
+
+ 
+
+export const getQuestions = async (difficulty: string) => { 
+
+  const response = await axios.get( 
+
+    "https://the-trivia-api.com/api/questions", 
+
+    { 
+
+      params: { 
+
+        limit: 5, 
+
+        difficulty: difficulty, 
+
+    } 
+
+  } 
+
+  ); 
+
+  const data = response.data; 
+
+  return data; 
+
+} 
+
+ 
+
+export const submitGame = async (UserId: string, Score: number) => { 
+
+  try { 
+
+  const data = await axios.post<GameProps>( 
+
+    baseUrl, 
+
+    { 
+
+      UserId: UserId, 
+
+      Score: Score 
+
+    }, 
+
+    { 
+
+      withCredentials: true, 
+
+      headers: { 
+
+        "Content-Type": "application/json", 
+
+        Accept: "application/json", 
+
+      }, 
+
+    }); 
+
+    return data; 
+
+  } catch (error: unknown) { 
+
+    if (axios.isAxiosError(error)) { 
+
+      const errMessage = error.response?.data.message; 
+
+      throw new Error(errMessage) 
+
+    } else { 
+
+      throw new Error('An unexpected error occurred'); 
+
+    } 
+
+  } 
+
+   
+
+} 
