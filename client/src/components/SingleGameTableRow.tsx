b8fc22c22c167b5206fae8ffb199a16cba22ef22
somych1 <samatbaltin@Samats-MacBook-Pro.local>
2024-05-03 12:26:14 -0500

    leaderboard styling

--- client/src/components/SingleGameTableRow.tsx
+++ client/src/components/SingleGameTableRow.tsx
@@ -9,16 +9,16 @@ interface propsInterface {
 export function SingleGameTableRow(props: propsInterface) {
     let dateElement;
     if (props.data.playedAt === null) {
-        dateElement = <td className="py-2 px-4"></td>
+        dateElement = <td className="py-2 px-4 text-yellow-800"></td>
     } else {
-        dateElement = <td className="py-2 px-4">{props.data.playedAt.substring(0, 10)}</td>
+        dateElement = <td className="py-2 px-4 text-yellow-800">{props.data.playedAt.substring(0, 10)}</td>
     }
     return (
         <>
         <tr>
-            <th className="py-2 px-4">{props.place}</th>
-            <td className="py-2 px-4">{props.data.createdBy.name}</td>
-            <td className="py-2 px-4">{props.data.score}</td>
+            <th className="py-2 px-4 text-yellow-800">{props.place}</th>
+            <td className="py-2 px-4 text-yellow-800">{props.data.createdBy.name}</td>
+            <td className="py-2 px-4 text-yellow-800">{props.data.score}</td>
             {dateElement}
         </tr>
         </>
